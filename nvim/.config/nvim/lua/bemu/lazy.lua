53bac96e40b501476735fecf44997054c8459b09
Bernd Müller <github@muellerbernd.de>
2023-09-08 11:26:25 +0200

    nvim: add surround plugin

--- nvim/.config/nvim/lua/bemu/lazy.lua
+++ nvim/.config/nvim/lua/bemu/lazy.lua
@@ -33,7 +33,18 @@ require('lazy').setup({
       }
     end,
   },
-  --
+  -- nvim-surround
+  {
+    'kylechui/nvim-surround',
+    version = '*', -- Use for stability; omit to use `main` branch for the latest features
+    event = 'VeryLazy',
+    config = function()
+      require('nvim-surround').setup {
+        -- Configuration here, or leave empty to use defaults
+      }
+    end,
+  },
+
   -- A high-performance color highlighter
   {
     'norcalli/nvim-colorizer.lua',
