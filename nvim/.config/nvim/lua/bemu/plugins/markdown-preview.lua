688fc929f392ef21fe708633ac18bfe2c229fbcf
Bernd Müller <github@muellerbernd.de>
2023-10-16 09:33:56 +0200

    nvim: comment out md preview

--- nvim/.config/nvim/lua/bemu/plugins/markdown-preview.lua
+++ nvim/.config/nvim/lua/bemu/plugins/markdown-preview.lua
@@ -1,6 +1,10 @@
 return {
-  'iamcco/markdown-preview.nvim',
-  config = function()
-    vim.fn['mkdp#util#install']()
-  end,
+  -- 'iamcco/markdown-preview.nvim',
+  -- config = function()
+  --   vim.fn['mkdp#util#install']()
+  -- end,
+  -- 'FelipeIzolan/markviewer.nvim',
+  -- config = function()
+  --   require('markviewer').setup()
+  -- end,
 }
