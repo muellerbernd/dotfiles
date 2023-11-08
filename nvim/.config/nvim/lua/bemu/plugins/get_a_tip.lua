a883d41c2097a919f52937104a06edace406b41e
Bernd Müller <github@muellerbernd.de>
2023-11-08 20:11:19 +0100

    nvim:

---
+++ nvim/.config/nvim/lua/bemu/plugins/get_a_tip.lua
@@ -0,0 +1,25 @@
+return {
+--   {
+--     "rcarriga/nvim-notify",
+--     init = function()
+--       vim.opt.termguicolors = true -- Enable 24-bit RGB color in the TUI
+--       vim.notify = require 'notify'
+--     end,
+--     config = function(_, opts)
+--       local job = require("plenary.job")
+--       job
+--         :new({
+--           command = "curl",
+--           args = { "https://vtip.43z.one" },
+--           on_exit = function(j, exit_code)
+--             local res = table.concat(j:result())
+--             if exit_code ~= 0 then
+--               res = "Error fetching tip: " .. res
+--             end
+--             require("notify")(res)
+--           end,
+--         })
+--         :start()
+--     end,
+--   },
+}
