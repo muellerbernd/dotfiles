695c37da05c249e8dbd7c7238569c3ef1c30d0b5
Bernd Müller <github@muellerbernd.de>
2023-11-13 09:46:04 +0100

    nvim: get a handy tip on start

--- nvim/.config/nvim/lua/bemu/plugins/get_a_tip.lua
+++ nvim/.config/nvim/lua/bemu/plugins/get_a_tip.lua
@@ -1,25 +1,30 @@
 return {
---   {
---     "rcarriga/nvim-notify",
---     init = function()
---       vim.opt.termguicolors = true -- Enable 24-bit RGB color in the TUI
---       vim.notify = require 'notify'
---     end,
---     config = function(_, opts)
---       local job = require("plenary.job")
---       job
---         :new({
---           command = "curl",
---           args = { "https://vtip.43z.one" },
---           on_exit = function(j, exit_code)
---             local res = table.concat(j:result())
---             if exit_code ~= 0 then
---               res = "Error fetching tip: " .. res
---             end
---             require("notify")(res)
---           end,
---         })
---         :start()
---     end,
---   },
+  {
+    -- idea taken from here
+    -- https://www.reddit.com/r/neovim/comments/17qdqkt/get_a_handy_tip_when_you_launch_neovim/
+    'rcarriga/nvim-notify',
+    init = function()
+      vim.opt.termguicolors = true -- Enable 24-bit RGB color in the TUI
+      vim.notify = require 'notify'
+    end,
+    config = function(_, opts)
+      require('notify').setup {
+        background_colour = '#000000',
+      }
+      local job = require 'plenary.job'
+      job
+        :new({
+          command = 'curl',
+          args = { 'https://vtip.43z.one' },
+          on_exit = function(j, exit_code)
+            local res = table.concat(j:result())
+            if exit_code ~= 0 then
+              res = 'Error fetching tip: ' .. res
+            end
+            require 'notify'(res)
+          end,
+        })
+        :start()
+    end,
+  },
 }
