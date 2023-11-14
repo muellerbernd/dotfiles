dbd482a8785cc423520e1053e4d2572a49cf089c
Bernd Müller <github@muellerbernd.de>
2023-11-14 12:40:02 +0100

    use neoformatter

--- nvim/.config/nvim/lua/bemu/plugins/formatting.lua
+++ nvim/.config/nvim/lua/bemu/plugins/formatting.lua
@@ -1,42 +1,78 @@
 return {
-  'stevearc/conform.nvim',
+  -- 'stevearc/conform.nvim',
+  -- lazy = true,
+  -- event = { 'BufRead', 'BufNewFile' }, -- to disable, comment this out
+  -- config = function()
+  --   local conform = require 'conform'
+  --
+  --   conform.setup {
+  --     formatters_by_ft = {
+  --       javascript = { 'prettier' },
+  --       typescript = { 'prettier' },
+  --       javascriptreact = { 'prettier' },
+  --       typescriptreact = { 'prettier' },
+  --       svelte = { 'prettier' },
+  --       css = { 'prettier' },
+  --       html = { 'prettier' },
+  --       json = { 'prettier' },
+  --       yaml = { 'prettier' },
+  --       markdown = { 'prettier' },
+  --       graphql = { 'prettier' },
+  --       lua = { 'stylua' },
+  --       python = { 'isort', 'black' },
+  --       nix = { 'nixfmt' },
+  --       bash = { 'beautysh' },
+  --       sh = { 'beautysh' },
+  --     },
+  --     -- format_on_save = {
+  --     --   lsp_fallback = true,
+  --     --   async = false,
+  --     --   timeout_ms = 1000,
+  --     -- },
+  --   }
+  --
+  --   vim.keymap.set({ 'n', 'v' }, '<F3>', function()
+  --     conform.format {
+  --       lsp_fallback = true,
+  --       async = false,
+  --       timeout_ms = 1000,
+  --     }
+  --   end, { desc = 'Format file or range (in visual mode)' })
+  -- end,
+
+  'sbdchd/neoformat',
   lazy = true,
-  event = { 'BufReadPre', 'BufNewFile' }, -- to disable, comment this out
+  event = { 'BufRead', 'BufNewFile' }, -- to disable, comment this out
   config = function()
-    local conform = require 'conform'
+    -- Enable alignment
+    vim.g.neoformat_basic_format_align = 1
 
-    conform.setup {
-      formatters_by_ft = {
-        javascript = { 'prettier' },
-        typescript = { 'prettier' },
-        javascriptreact = { 'prettier' },
-        typescriptreact = { 'prettier' },
-        svelte = { 'prettier' },
-        css = { 'prettier' },
-        html = { 'prettier' },
-        json = { 'prettier' },
-        yaml = { 'prettier' },
-        markdown = { 'prettier' },
-        graphql = { 'prettier' },
-        lua = { 'stylua' },
-        python = { 'isort', 'black' },
-        nix = { 'nixfmt' },
-        bash = { 'beautysh' },
-        sh = { 'beautysh' },
-      },
-      -- format_on_save = {
-      --   lsp_fallback = true,
-      --   async = false,
-      --   timeout_ms = 1000,
-      -- },
-    }
+    -- Enable tab to spaces conversion
+    vim.g.neoformat_basic_format_retab = 1
 
-    vim.keymap.set({ 'n', 'v' }, '<F3>', function()
-      conform.format {
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 1000,
-      }
-    end, { desc = 'Format file or range (in visual mode)' })
+    -- Enable trimmming of trailing whitespace
+    vim.g.neoformat_basic_format_trim = 1
+
+    -- run all formatters
+    vim.g.neoformat_run_all_formatters = 0
+    -- Have Neoformat only msg when there is an error
+    -- vim.g.neoformat_only_msg_on_error = 1
+    --
+    vim.g.neoformat_enabled_python = { 'isort', 'black' }
+    vim.g.neoformat_enabled_rust = { 'rustfmt' }
+    vim.g.neoformat_enabled_lua = { 'stylua' }
+    vim.g.neoformat_enabled_yaml = { 'prettier' }
+    vim.g.neoformat_enabled_json = { 'prettier' }
+    vim.g.neoformat_enabled_css = { 'prettier' }
+    vim.g.neoformat_enabled_html = { 'prettier' }
+    -- vim.g.neoformat_enabled_handlebars = {}
+    vim.g.neoformat_enabled_xml = { 'tidy' }
+    vim.g.neoformat_enabled_shell = { 'beautysh' }
+    vim.g.neoformat_enabled_bash = { 'beautysh' }
+    vim.g.neoformat_enabled_haskell = { 'ormolu' }
+    vim.g.neoformat_enabled_markdown = { 'prettier' }
+    vim.g.neoformat_enabled_nix = { 'nixfmt' }
+
+    vim.keymap.set({ 'n', 'v' }, '<F3>', '<cmd>Neoformat<cr>', { desc = 'Format file or range (in visual mode)' })
   end,
 }
