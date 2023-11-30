fcfc5fb20b3f660c7d063d442da1e3982fd2a8b7
Bernd Müller <github@muellerbernd.de>
2023-11-30 08:43:17 +0100

    scripts: rofi music

--- scripts/scripts/rofi-music.sh
+++ scripts/scripts/rofi-music.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 
+DIR="$(cd "$(dirname "$0")" && pwd)"
 if ! command -v rofi-music-rs &> /dev/null
 then
     echo "rofi-music-rs could not be found"
-    ./rofi-music
+    $DIR/rofi-music
     # exit 1
 else
     rofi-music-rs
