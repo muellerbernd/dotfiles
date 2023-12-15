29cffeb22fa207dd6e977764709be178fb9e32c6
Bernd Müller <github@muellerbernd.de>
2023-12-15 10:44:01 +0100

    nvim: activate markdown-preview

--- nvim/.config/nvim/lua/bemu/plugins/markdown-preview.lua
+++ nvim/.config/nvim/lua/bemu/plugins/markdown-preview.lua
@@ -1,10 +1,24 @@
 return {
-  -- 'iamcco/markdown-preview.nvim',
-  -- config = function()
-  --   vim.fn['mkdp#util#install']()
-  -- end,
+  -- {
+  --   'iamcco/markdown-preview.nvim',
+  --   cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+  --   ft = { 'markdown' },
+  --   build = function()
+  --     vim.fn['mkdp#util#install']()
+  --   end,
+  -- },
+  {
+    'iamcco/markdown-preview.nvim',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    build = 'cd app && yarn install',
+    init = function()
+      vim.g.mkdp_filetypes = { 'markdown' }
+    end,
+    ft = { 'markdown' },
+  }
   -- 'FelipeIzolan/markviewer.nvim',
   -- config = function()
   --   require('markviewer').setup()
   -- end,
+  -- { 'ellisonleao/glow.nvim', config = true, cmd = 'Glow' },
 }
