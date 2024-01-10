961d662c3338e85ce5ff2be1fac27819378eda1c
Bernd Müller <github@muellerbernd.de>
2024-01-10 10:46:56 +0100

    hyprland cfg

--- scripts/scripts/notifyCaps.sh
+++ scripts/scripts/notifyCaps.sh
@@ -1,16 +1,10 @@
-#!/bin/bash
-# File              : notifyCaps.sh
-# Author            : Bernd Müller <bernd@muellerbernd.de>
-# Date              : 28.06.2023
+#!/usr/bin/env bash
 msgId="981141"
-caps_lock_status=$(xset -q | sed -n 's/^.*Caps Lock:\s*\(\S*\).*$/\1/p')
-echo "$caps_lock_status"
-dunstify -u low -r "$msgId" "caps lock $caps_lock_status"
-# if [ $caps_lock_status == "on" ]; then
-#   echo "Caps lock on"
-#   dunstify -u low -r "$msgId" "caps lock on"
-#   # xdotool key Caps_Lock
-# else
-#   echo "Caps lock off"
-#   dunstify -u low -r "$msgId" "caps lock off"
-# fi
+
+caps_lock_status=$(cat /sys/class/leds/input0::capslock/brightness)
+
+if [ $caps_lock_status == "1" ]; then
+  dunstify -u low -r "$msgId" "caps lock on"
+else
+  dunstify -u low -r "$msgId" "caps lock off"
+fi
