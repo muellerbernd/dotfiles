681ee0b443a9084a5a67e8164dba64a887824281
Bernd Müller <github@muellerbernd.de>
2024-01-10 17:57:14 +0100

    hyprland: idle script

--- hypr-common/.config/hypr/scripts/idle.sh
+++ hypr-common/.config/hypr/scripts/idle.sh
@@ -2,6 +2,6 @@
 #
 exec swayidle -w \
     timeout 300 'notify-send -u critical -t 9000 "Locking Screen in 10 seconds"' \
-    timeout 310 '~/scripts/lock_sway.sh' \
+    timeout 310 '~/.config/hypr/scripts/lock.sh' \
     timeout 500 'hyprctl dispatch dpms off'\
     resume 'hyprctl dispatch dpms on'
