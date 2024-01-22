d33bad4f61fb38189eb54e93e98de61eea62ea9c
Bernd Müller <github@muellerbernd.de>
2024-01-22 22:08:36 +0100

    update hyprland

--- hypr-common/.config/hypr/scripts/xdgAtomic.sh
+++ hypr-common/.config/hypr/scripts/xdgAtomic.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
-sleep 1
-killall -e xdg-desktop-portal-hyprland
+# sleep 1
+# killall -e xdg-desktop-portal-hyprland
+# killall -e xdg-desktop-portal-wlr
+# killall xdg-desktop-portal
+# /usr/lib/xdg-desktop-portal-hyprland &
+# sleep 2
+# /usr/lib/xdg-desktop-portal &
+sleep 4
 killall -e xdg-desktop-portal-wlr
 killall xdg-desktop-portal
-/usr/lib/xdg-desktop-portal-hyprland &
-sleep 2
+/usr/lib/xdg-desktop-portal-wlr &
+sleep 4
 /usr/lib/xdg-desktop-portal &
