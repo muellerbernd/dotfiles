e2c2cb84ef78d33dbc415ce726c1147be0d2eddc
Bernd Müller <github@muellerbernd.de>
2024-02-09 21:24:54 +0100

    hyprland: increase idle timeout, add keybinding to move workspace onto monitor

--- hypr-common/.config/hypr/scripts/idle.sh
+++ hypr-common/.config/hypr/scripts/idle.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 swayidle -w \
-    timeout 300 'notify-send -u critical -t 9000 "Locking Screen in 10 seconds"' \
-    timeout 310 'hyprctl dispatch exec ~/.config/hypr/scripts/lock.sh' \
-    timeout 500 'hyprctl dispatch dpms off'\
+    timeout 600 'notify-send -u critical -t 9000 "Locking Screen in 10 seconds"' \
+    timeout 610 'hyprctl dispatch exec ~/.config/hypr/scripts/lock.sh' \
+    timeout 900 'hyprctl dispatch dpms off'\
     resume 'hyprctl dispatch dpms on'
