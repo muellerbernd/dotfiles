2b67bb60f1e66603a35dbafbcb808972bcd880fb
Bernd Müller <github@muellerbernd.de>
2024-03-01 23:52:41 +0100

    hyprland: wallpaper script
    Signed-off-by: Bernd Müller <github@muellerbernd.de>

--- hypr-common/.config/hypr/scripts/wallpaper.sh
+++ hypr-common/.config/hypr/scripts/wallpaper.sh
@@ -2,8 +2,8 @@
 
 HOSTNAME=$(hostname)
 
-if [[ $HOSTNAME == "mue-p14s" ]]; then
-    swaybg -m fill -i ~/wallpapers/work-wallpaper.png
-else
+# if [[ $HOSTNAME == "mue-p14s" ]]; then
+#     swaybg -m fill -i ~/wallpapers/work-wallpaper.png
+# else
     swaybg -m fill -i ~/wallpapers/black-hole.png
-fi
+# fi
