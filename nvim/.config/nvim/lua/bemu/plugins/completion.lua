0b011ae09fd3eeaaee2b1664a3045eedb337cd1c
Bernd Müller <github@muellerbernd.de>
2024-07-02 16:29:32 +0200

    [refactor] nvim: cleanup

--- nvim/.config/nvim/lua/bemu/plugins/completion.lua
+++ nvim/.config/nvim/lua/bemu/plugins/completion.lua
@@ -117,8 +117,9 @@ return {
         -- ["<C-Space>"] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.close(),
         ['<CR>'] = cmp.mapping.confirm {
-          behavior = cmp.ConfirmBehavior.Replace,
-          -- select = true,
+          -- behavior = cmp.ConfirmBehavior.Replace,
+          behavior = cmp.ConfirmBehavior.Insert,
+          select = true,
         },
       },
       sources = cmp.config.sources {
