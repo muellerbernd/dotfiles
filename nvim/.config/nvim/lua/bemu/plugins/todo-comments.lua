8e4d0932bd85b41b86172d9d6d4bea3f77f4c0e0
Bernd Müller <github@muellerbernd.de>
2024-07-03 14:38:09 +0200

    [change] nvim: todo-comments search command

--- nvim/.config/nvim/lua/bemu/plugins/todo-comments.lua
+++ nvim/.config/nvim/lua/bemu/plugins/todo-comments.lua
@@ -1,10 +1,21 @@
 return {
   'folke/todo-comments.nvim',
-  event = { 'BufReadPre', 'BufNewFile' },
   -- config = true,
   dependencies = 'nvim-lua/plenary.nvim',
   config = function()
-    require('todo-comments').setup {}
+    require('todo-comments').setup {
+      search = {
+        command = 'rg',
+        args = {
+          '--color=never',
+          '--no-heading',
+          '--with-filename',
+          '--line-number',
+          '--column',
+          '--no-ignore-vcs',
+        },
+      },
+    }
     -- todo
     vim.keymap.set('n', '<leader>td', '<cmd>TodoTelescope<CR>', { desc = 'list current [t]o[d]os' })
   end,
