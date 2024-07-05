7c0f40f6cc50f18112bb0024b055a3e132e25152
Bernd Müller <github@muellerbernd.de>
2024-07-05 12:50:30 +0200

    [add] nvim: typst formatter

--- nvim/.config/nvim/lua/bemu/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/bemu/plugins/none-ls.lua
@@ -9,6 +9,7 @@ return {
         null_ls.builtins.formatting.black,
         null_ls.builtins.formatting.isort,
         null_ls.builtins.formatting.shfmt,
+        null_ls.builtins.formatting.typstfmt,
         null_ls.builtins.formatting.prettier.with {
           filetypes = { 'html', 'json', 'markdown' },
         },
