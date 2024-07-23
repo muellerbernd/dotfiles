71d23310719bb04dfe6e745e1a6010b891faff64
Bernd Müller <github@muellerbernd.de>
2024-07-23 16:04:31 +0200

    [add] nvim: lazydev

--- nvim/.config/nvim/lua/bemu/plugins/lsp/init.lua
+++ nvim/.config/nvim/lua/bemu/plugins/lsp/init.lua
@@ -9,9 +9,6 @@ return {
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
       { 'j-hui/fidget.nvim', opts = {} },
 
-      -- `neodev` configures Lua LSP for your Neovim config, runtime and plugins
-      -- used for completion, annotations and signatures of Neovim apis
-      -- { 'folke/neodev.nvim',                   opts = {} },
     },
     config = function()
       _ = require 'bemu.plugins.lsp.handlers'
