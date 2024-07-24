4bb304a8cf04b47323489e03d057188b1efc7a3f
Bernd Müller <github@muellerbernd.de>
2024-07-24 23:32:26 +0200

    [change] niri: config, env vars

--- rofi/.config/rofi/scripts/powermenu/run.sh
+++ rofi/.config/rofi/scripts/powermenu/run.sh
@@ -92,6 +92,8 @@ run_cmd() {
                 sway-msg exit
             elif [[ "$XDG_CURRENT_DESKTOP" == 'Hyprland' ]]; then
                 hyprctl dispatch exit
+            elif [[ "$XDG_CURRENT_DESKTOP" == 'niri' ]]; then
+                niri msg action quit
             fi
         fi
     else
