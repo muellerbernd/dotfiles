59974b76ba1465dd6b262956275e41d142244c10
Bernd Müller <github@muellerbernd.de>
2024-08-23 09:28:41 +0200

    [change] nvim: lsp nixd

--- nvim/.config/nvim/lua/bemu/plugins/lsp/init.lua
+++ nvim/.config/nvim/lua/bemu/plugins/lsp/init.lua
@@ -8,7 +8,6 @@ return {
       -- Useful status updates for LSP.
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
       { 'j-hui/fidget.nvim', opts = {} },
-
     },
     config = function()
       _ = require 'bemu.plugins.lsp.handlers'
@@ -229,8 +228,19 @@ return {
             capabilities = capabilities,
             settings = {
               ['nixd'] = {
+                nixpkgs = {
+                  expr = 'import <nixpkgs> { }',
+                },
                 formatting = {
                   command = { 'alejandra' },
+                },
+                options = {
+                  nixos = {
+                    expr = '(builtins.getFlake ("git+file://" + toString ./.)).nixosConfigurations.ammerapad.options',
+                  },
+                  home_manager = {
+                    expr = '(builtins.getFlake ("git+file://" + toString ./.)).homeConfigurations."bernd@ammerapad".options',
+                  },
                 },
               },
             },
