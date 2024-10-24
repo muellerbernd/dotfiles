58d0f3ec50d2c62d116c5f105eea6358e6fe4a7b
Bernd Müller <github@muellerbernd.de>
2024-10-24 07:58:09 +0200

    [change] nvim: wiki list

--- nvim/.config/nvim/lua/bemu/core/wiki.lua
+++ nvim/.config/nvim/lua/bemu/core/wiki.lua
@@ -3,6 +3,6 @@ vim.g.mywiki_list = {
     path = '/home/bernd/Desktop/Nextcloud/mywiki/index.md',
   },
   {
-    path = '/home/bernd/Desktop/GitProjects/IOSB-WORK/wiki/index.md',
+    path = '/home/bernd/SyncWork/wiki',
   },
 }
