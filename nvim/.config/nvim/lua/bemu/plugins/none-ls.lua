605c7a4259af0248de727a570be578d3dff5fb33
Bernd Müller <github@muellerbernd.de>
2025-01-08 10:51:17 +0100

    [change] fish config, nvim: add formatting for fish

--- nvim/.config/nvim/lua/bemu/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/bemu/plugins/none-ls.lua
@@ -20,6 +20,7 @@ return {
 
         -- null_ls.builtins.diagnostics.write_good,
         -- null_ls.builtins.code_actions.gitsigns,
+        null_ls.builtins.formatting.fish_indent,
       },
     }
 
