1574e5f51aaf3b2b039c9bc6b26df44f35bd0998
Bernd Müller <github@muellerbernd.de>
2025-01-08 10:09:52 +0100

    [add] fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -20,8 +20,7 @@ end
 
 # aliases
 alias tm="tmux a || tmux"
-alias l='eza -l --git -O'
-alias -g nvidia_offload='__NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME="nvidia" __VK_LAYER_NV_optimus="NVIDIA_only"'
+alias l="eza -l --git -O"
 
 # set env vars
 export FZF_DEFAULT_COMMAND="fd --color=always --strip-cwd-prefix --hidden --follow --exclude .git"
