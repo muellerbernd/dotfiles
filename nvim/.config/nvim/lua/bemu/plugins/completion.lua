dd8d25730cf75fe9adef0c4bd494dcde29a5696a
Bernd Müller <github@muellerbernd.de>
2025-02-27 15:59:51 +0100

    [change] nvim: blink.cmp settings

--- nvim/.config/nvim/lua/bemu/plugins/completion.lua
+++ nvim/.config/nvim/lua/bemu/plugins/completion.lua
@@ -1,5 +1,6 @@
 return {
   'saghen/blink.cmp',
+  event = 'InsertEnter',
   -- optional: provides snippets for the snippet source
   dependencies = {
     'onsails/lspkind.nvim',
@@ -39,6 +40,7 @@ return {
       ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
       ['<C-e>'] = { 'hide' },
       ['<C-y>'] = { 'select_and_accept' },
+      ['<CR>'] = { 'select_and_accept' },
 
       ['<C-p>'] = { 'select_prev', 'fallback' },
       ['<C-n>'] = { 'select_next', 'fallback' },
@@ -46,59 +48,17 @@ return {
       ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
       ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
 
-      ['<Tab>'] = { 'snippet_forward', 'fallback' },
-      ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
-    },
-
-    -- default list of enabled providers defined so that you can extend it
-    -- elsewhere in your config, without redefining it, via `opts_extend`
-    sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
-      -- optionally disable cmdline completions
-      cmdline = function()
-        local type = vim.fn.getcmdtype()
-        -- Search forward and backward
-        if type == '/' or type == '?' then
-          return { 'buffer' }
-        end
-        -- Commands
-        if type == ':' then
-          return { 'cmdline' }
-        end
-        return {}
-      end,
-    },
-    snippets = { preset = 'luasnip' },
-
-    -- experimental signature help support
-    signature = { enabled = true },
-
-    appearance = {
-      use_nvim_cmp_as_default = false,
-      nerd_font_variant = 'mono',
+      ['<Tab>'] = { 'select_next', 'snippet_forward', 'fallback' },
+      ['<S-Tab>'] = { 'select_prev', 'snippet_backward', 'fallback' },
     },
     completion = {
-      ghost_text = { enabled = true },
-      accept = { auto_brackets = { enabled = false } },
-      documentation = {
-        auto_show = true,
-        auto_show_delay_ms = 250,
-        update_delay_ms = 50,
-        treesitter_highlighting = true,
-        window = { border = 'rounded' },
+      list = {
+        -- Insert items while navigating the completion list.
+        selection = { preselect = false, auto_insert = true },
+        max_items = 10,
       },
-      list = { selection = { preselect = false, auto_insert = false } },
       menu = {
-        auto_show = true,
         border = 'rounded',
-        cmdline_position = function()
-          if vim.g.ui_cmdline_pos ~= nil then
-            local pos = vim.g.ui_cmdline_pos -- (1, 0)-indexed
-            return { pos[1] - 1, pos[2] }
-          end
-          local height = (vim.o.cmdheight == 0) and 1 or vim.o.cmdheight
-          return { vim.o.lines - height, 0 }
-        end,
         draw = {
           columns = {
             { 'kind_icon', 'label', gap = 1 },
@@ -127,9 +87,41 @@ return {
           },
         },
       },
+      documentation = {
+        auto_show = true,
+        auto_show_delay_ms = 250,
+        update_delay_ms = 50,
+        treesitter_highlighting = true,
+        window = { border = 'rounded' },
+      },
+    },
+    snippets = { preset = 'luasnip' },
+    -- experimental signature help support
+    signature = { enabled = true },
+    -- Disable command line completion:
+    cmdline = { enabled = false },
+    sources = {
+
+      -- Disable some sources in comments and strings.
+      default = function()
+        local sources = { 'lsp', 'snippets', 'buffer' }
+        local ok, node = pcall(vim.treesitter.get_node)
+
+        if ok and node then
+          if not vim.tbl_contains({ 'comment', 'line_comment', 'block_comment' }, node:type()) then
+            table.insert(sources, 'path')
+          end
+          if node:type() ~= 'string' then
+            table.insert(sources, 'snippets')
+          end
+        end
+
+        return sources
+      end,
+    },
+    appearance = {
+      use_nvim_cmp_as_default = false,
+      nerd_font_variant = 'mono',
     },
   },
-  -- allows extending the providers array elsewhere in your config
-  -- without having to redefine it
-  opts_extend = { 'sources.default' },
 }
