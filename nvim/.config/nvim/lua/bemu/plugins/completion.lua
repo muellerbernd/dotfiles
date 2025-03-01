0bda3ce868824f0ba1d6ba94ac9b2d75fdaef921
Bernd Müller <github@muellerbernd.de>
2025-03-01 06:35:26 +0100

    [change] nvim: blink.cmp settings

--- nvim/.config/nvim/lua/bemu/plugins/completion.lua
+++ nvim/.config/nvim/lua/bemu/plugins/completion.lua
@@ -98,7 +98,40 @@ return {
     -- experimental signature help support
     signature = { enabled = true },
     -- Disable command line completion:
-    cmdline = { enabled = true },
+    cmdline = {
+      enabled = true,
+      keymap = { preset = 'cmdline' },
+      sources = function()
+        local type = vim.fn.getcmdtype()
+        -- Search forward and backward
+        if type == '/' or type == '?' then
+          return { 'buffer' }
+        end
+        -- Commands
+        if type == ':' or type == '@' then
+          return { 'cmdline' }
+        end
+        return {}
+      end,
+      completion = {
+        trigger = {
+          show_on_blocked_trigger_characters = {},
+          show_on_x_blocked_trigger_characters = {},
+        },
+        list = {
+          selection = {
+            -- When `true`, will automatically select the first item in the completion list
+            preselect = true,
+            -- When `true`, inserts the completion item automatically when selecting it
+            auto_insert = true,
+          },
+        },
+        -- Whether to automatically show the window when new completion items are available
+        menu = { auto_show = true },
+        -- Displays a preview of the selected item on the current line
+        ghost_text = { enabled = true },
+      },
+    },
     sources = {
 
       -- Disable some sources in comments and strings.
