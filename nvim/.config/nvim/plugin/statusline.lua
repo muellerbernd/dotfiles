20b69a924d26a1ffe8848489af1a37b275500840
Bernd Müller <github@muellerbernd.de>
2025-03-17 10:56:34 +0100

    [change] nvim: statusline

--- nvim/.config/nvim/plugin/statusline.lua
+++ nvim/.config/nvim/plugin/statusline.lua
@@ -332,40 +332,100 @@ _G.statusline = M
 
 return M
 
--- more simple version based on https://www.reddit.com/r/neovim/comments/1jbzvtb/i_learned_how_to_customize_the_statusline/
--- function update_statusline()
---   vim.system({ 'git', 'rev-parse', '--abbrev-ref', 'HEAD' }, { text = true }, function(res)
---     vim.schedule(function()
---       -- there should be here a "branch" icon but reddit doesn't render it
---       local branch = ' ' .. vim.trim(res.stdout)
+-- local group = vim.api.nvim_create_augroup("StatusLine", { clear = true })
 --
---       vim.o.statusline = " %f %{&modified?'●':''} " .. branch .. '%=at %c | %L lines | %%%p '
+-- local icons = {
+--     diagnostics = {
+--         error = "",
+--         warning = "",
+--         hint = "",
+--         info = "",
+--     },
+--     buffers = {
+--         readonly = "󰌾",
+--         modified = "●",
+--         unsaved_others = "○",
+--     },
+-- }
 --
---       vim.cmd 'redrawstatus'
+-- local diagnostics_attrs = {
+--     { "Error", icons.diagnostics.error },
+--     { "Warn", icons.diagnostics.warning },
+--     { "Hint", icons.diagnostics.hint },
+--     { "Info", icons.diagnostics.info },
+-- }
+--
+-- local diagnostics = ""
+-- local function diagnostics_section()
+--     local results = {}
+--
+--     for _, attr in pairs(diagnostics_attrs) do
+--         local n = vim.diagnostic.get(0, { severity = attr[1] })
+--         if #n > 0 then
+--             table.insert(results, string.format(" %d %s", #n, attr[2]))
+--         end
 --     end
---     )
---   end)
+--
+--     return table.concat(results)
 -- end
 --
--- vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'ShellCmdPost' }, {
---
---   pattern = '*',
---
---   callback = function()
---     local filename = vim.fn.bufname '%'
---
---     local buftype = vim.bo.buftype
---
---     -- local is\_file\_valid = vim.fn.filereadable(filename)
---
---     if filename == '' or buftype ~= '' then
---       vim.schedule(function()
---         vim.opt_local.statusline = ' '
---       end)
---     else
---       update_statusline()
---     end
---   end,
+-- vim.api.nvim_create_autocmd({ "DiagnosticChanged", "BufWinEnter" }, {
+--     group = group,
+--     callback = function()
+--         diagnostics = diagnostics_section()
+--     end,
 -- })
 --
--- vim.o.statusline = " %f %{&modified?'●':''}%=at %c | %L lines | %%%p "
+-- local function unsaved_buffers()
+--     for _, buf in pairs(vim.api.nvim_list_bufs()) do
+--         if vim.api.nvim_get_current_buf() == buf then
+--             goto continue
+--         end
+--         if vim.api.nvim_buf_get_option(buf, "modified") then
+--             return string.format(" %s ", icons.buffers.unsaved_others)
+--         end
+--         ::continue::
+--     end
+--
+--     return ""
+-- end
+--
+-- local function file_section()
+--     local name, ext = vim.fn.expand "%:t", vim.fn.expand "%:e"
+--     local attr, icon = "", ""
+--
+--     local ok, nvim_devicons = pcall(require, "nvim-web-devicons")
+--     if ok then
+--         icon = nvim_devicons.get_icon("", ext, { default = true })
+--     end
+--
+--     if vim.bo.modified and vim.bo.readonly then
+--         attr = icons.buffers.modified .. " " .. icons.buffers.readonly
+--     elseif vim.bo.readonly then
+--         attr = icons.buffers.readonly
+--     elseif vim.bo.modified then
+--         attr = icons.buffers.modified
+--     end
+--
+--     if attr ~= "" then
+--         attr = " " .. attr
+--     end
+--
+--     if name == "" then
+--         name = "No Name"
+--     end
+--
+--     return string.format("%s %s%s", icon, name, attr)
+-- end
+--
+-- local function left_section()
+--     return file_section() .. unsaved_buffers() .. diagnostics
+-- end
+--
+-- local function right_section()
+--     return "%l:%L"
+-- end
+--
+-- _G.set_statusline = function()
+--     return " " .. left_section() .. "%=" .. right_section() .. " "
+-- end
