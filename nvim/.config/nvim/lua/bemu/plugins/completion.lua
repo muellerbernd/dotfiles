36c1a5f9a8b55b1d2f3cea49a87bd21d042b3434
Bernd Müller <github@muellerbernd.de>
2025-03-26 06:31:41 +0100

    [fix] nvim: blink pin version 1.0

--- nvim/.config/nvim/lua/bemu/plugins/completion.lua
+++ nvim/.config/nvim/lua/bemu/plugins/completion.lua
@@ -24,7 +24,8 @@ return {
   },
   -- use a release tag to download pre-built binaries
   -- version = 'v0.*',
-  version = 'v0.13.0',
+  -- version = 'v0.13.0',
+  version = 'v1.0',
   -- version = 'main',
   -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
   -- build = 'cargo build --release',
