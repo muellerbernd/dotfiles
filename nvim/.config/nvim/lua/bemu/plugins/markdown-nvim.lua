568ad3d09f98e03fe84ce16a82dcba2d4e25fa34
Bernd Müller <github@muellerbernd.de>
2025-09-03 06:42:29 +0200

    [change] nvim: reactivate markdown-nvim

--- nvim/.config/nvim/lua/bemu/plugins/markdown-nvim.lua
+++ nvim/.config/nvim/lua/bemu/plugins/markdown-nvim.lua
@@ -1,15 +1,15 @@
 return {
-  -- 'MeanderingProgrammer/markdown.nvim',
-  -- main = 'render-markdown',
-  -- opts = {},
-  -- name = 'render-markdown',                             -- Only needed if you have another plugin named markdown.nvim
-  -- dependencies = { 'nvim-treesitter/nvim-treesitter' }, -- if you use the mini.nvim suite
-  -- -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
-  -- -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-  -- config = function()
-  --   require('render-markdown').setup {
-  --
-  --     latex = { enabled = false },
-  --   }
-  -- end,
+  'MeanderingProgrammer/markdown.nvim',
+  main = 'render-markdown',
+  opts = {},
+  name = 'render-markdown',                             -- Only needed if you have another plugin named markdown.nvim
+  dependencies = { 'nvim-treesitter/nvim-treesitter' }, -- if you use the mini.nvim suite
+  -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+  -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+  config = function()
+    require('render-markdown').setup {
+
+      latex = { enabled = false },
+    }
+  end,
 }
