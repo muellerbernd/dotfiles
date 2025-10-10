c27a40277ca5df2b88711267ea55e15940646a1b
Bernd Müller <github@muellerbernd.de>
2025-10-10 13:54:45 +0200

    [refactor] nvim: use conform as formatter

--- nvim/.config/nvim/lua/bemu/plugins/formatter.lua
+++ nvim/.config/nvim/lua/bemu/plugins/formatter.lua
@@ -1,53 +1,59 @@
 return {
-  'nvimtools/none-ls.nvim',
-  config = function()
-    local null_ls = require 'null-ls'
-    null_ls.setup {
-      sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettier,
-        null_ls.builtins.formatting.black,
-        null_ls.builtins.formatting.isort,
-        null_ls.builtins.formatting.shfmt,
-        -- null_ls.builtins.formatting.typstyle,
-        null_ls.builtins.formatting.prettier.with {
-          filetypes = { 'html', 'json', 'markdown' },
-        },
-        null_ls.builtins.formatting.yamlfmt.with {
-          filetypes = { 'yaml' },
-          extra_filetypes = { 'toml' },
-        },
-
-        -- null_ls.builtins.diagnostics.write_good,
-        -- null_ls.builtins.code_actions.gitsigns,
-        -- null_ls.builtins.formatting.fish_indent,
-      },
-    }
-
-    vim.keymap.set('n', '<space>ff', function()
-      vim.lsp.buf.format { async = true }
-    end, { desc = '[f]ormat [f]ile' })
-  end,
-  -- {
-  --   'stevearc/conform.nvim',
-  --   opts = {
-  --     notify_on_error = true,
-  --     -- format_on_save = {
-  --     --   timeout_ms = 500,
-  --     --   lsp_fallback = true,
-  --     -- },
-  --     formatters_by_ft = {
-  --       lua = { 'stylua', lsp_format = 'fallback' },
-  --       -- Conform will run multiple formatters sequentially
-  --       python = { 'black', 'isort', lsp_format = 'fallback' },
-  --       -- You can customize some of the format options for the filetype (:help conform.format)
-  --       -- rust = { 'rustfmt', lsp_format = 'fallback' },
-  --       -- Conform will run the first available formatter
-  --       -- javascript = { 'prettierd', 'prettier', stop_after_first = true },
+  -- 'nvimtools/none-ls.nvim',
+  -- config = function()
+  --   local null_ls = require 'null-ls'
+  --   null_ls.setup {
+  --     sources = {
+  --       null_ls.builtins.formatting.stylua,
+  --       null_ls.builtins.formatting.prettier,
+  --       null_ls.builtins.formatting.black,
+  --       null_ls.builtins.formatting.isort,
+  --       null_ls.builtins.formatting.shfmt,
+  --       -- null_ls.builtins.formatting.typstyle,
+  --       null_ls.builtins.formatting.prettier.with {
+  --         filetypes = { 'html', 'json', 'markdown' },
+  --       },
+  --       null_ls.builtins.formatting.yamlfmt.with {
+  --         filetypes = { 'yaml' },
+  --         extra_filetypes = { 'toml' },
+  --       },
+  --
+  --       -- null_ls.builtins.diagnostics.write_good,
+  --       -- null_ls.builtins.code_actions.gitsigns,
+  --       -- null_ls.builtins.formatting.fish_indent,
   --     },
-  --     vim.keymap.set('n', '<space>ff', function()
-  --       vim.lsp.buf.format { async = true }
-  --     end, { desc = '[f]ormat [f]ile' }),
-  --   },
+  --   }
+  --
+  --   vim.keymap.set('n', '<space>ff', function()
+  --     vim.lsp.buf.format { async = true }
+  --   end, { desc = '[f]ormat [f]ile' })
+  -- end,
+  -- {
+  'stevearc/conform.nvim',
+  opts = {
+    default_format_opts = {
+      lsp_format = 'fallback',
+    },
+    formatters_by_ft = {
+      -- bib = { 'bibtex-tidy' },
+      html = { 'prettier' },
+      lua = { 'stylua' },
+      markdown = { 'prettier' },
+      json = { 'prettier' },
+      python = { 'black', 'isort' },
+      -- tex = { 'tex-fmt' },
+      yaml = { 'yamlfmt' },
+      toml = { 'yamlfmt' },
+    },
+    formatters = {
+      -- ['tex-fmt'] = {
+      --   prepend_args = { '--nowrap', '--tabsize', '4' },
+      -- },
+    },
+    vim.keymap.set('n', '<space>ff', function()
+      -- vim.lsp.buf.format { async = true }
+      require("conform").format()
+    end, { desc = '[f]ormat [f]ile' }),
+  },
   -- }
 }
