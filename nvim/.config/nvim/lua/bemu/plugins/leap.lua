286b1493d8384ca4a92a7391758ba00b417e8634
Bernd Müller <github@muellerbernd.de>
2025-10-21 11:07:10 +0200

    [refactor] nvim: leap, disable autopairs

--- nvim/.config/nvim/lua/bemu/plugins/leap.lua
+++ nvim/.config/nvim/lua/bemu/plugins/leap.lua
@@ -1,19 +1,39 @@
 return {
-  {
-    'ggandor/leap.nvim',
-    config = function()
-      local leap = require 'leap'
-      leap.set_default_mappings()
-      leap.opts.preview_filter = function(ch0, ch1, ch2)
-        return not (ch1:match '%s' or ch0:match '%a' and ch1:match '%a' and ch2:match '%a')
-      end
-      leap.opts.case_sensitive = true
-      -- vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap-forward)')
-      -- vim.keymap.set({ 'n', 'x', 'o' }, 'S', '<Plug>(leap-backward)')
-      -- vim.keymap.set('n', 'gs', '<Plug>(leap-from-window)')
+  'ggandor/leap.nvim',
+  event = 'BufReadPost',
+  config = function()
+    local leap = require 'leap'
+    leap.add_default_mappings()
+    leap.opts.case_sensitive = true
+    leap.opts.max_phase_one_targets = 5
+    leap.opts.highlight_unlabeled_phase_one_targets = true
+    leap.opts.preview_filter = function(ch0, ch1, ch2)
+      return not (ch1:match '%s' or ch0:match '%w' and ch1:match '%w' and ch2:match '%w')
+    end
 
-      -- vim.keymap.set('n', 's', '<Plug>(leap-anywhere)')
-      -- vim.keymap.set({ 'x', 'o' }, 's', '<Plug>(leap)')
-    end,
-  },
+    -- Highly recommended: define a preview filter to reduce visual noise
+    -- and the blinking effect after the first keypress
+    -- (`:h leap.opts.preview`). You can still target any visible
+    -- positions if needed, but you can define what is considered an
+    -- exceptional case.
+    -- Exclude whitespace and the middle of alphabetic words from preview:
+    --   foobar[baaz] = quux
+    --   ^----^^^--^^-^-^--^
+    require('leap').opts.preview = function(ch0, ch1, ch2)
+      return not (ch1:match '%s' or (ch0:match '%a' and ch1:match '%a' and ch2:match '%a'))
+    end
+
+    -- Define equivalence classes for brackets and quotes, in addition to
+    -- the default whitespace group:
+    require('leap').opts.equivalence_classes = {
+      ' \t\r\n',
+      '([{',
+      ')]}',
+      '\'"`',
+    }
+
+    -- Use the traversal keys to repeat the previous motion without
+    -- explicitly invoking Leap:
+    require('leap.user').set_repeat_keys('<enter>', '<backspace>')
+  end,
 }
--- nvim/.config/nvim/lua/bemu/plugins/nvim-autopairs.lua
+++ nvim/.config/nvim/lua/bemu/plugins/nvim-autopairs.lua
@@ -1,25 +1,25 @@
 return {
-  'windwp/nvim-autopairs',
-  event = { 'InsertEnter' },
-  config = function()
-    local npairs = require 'nvim-autopairs'
-    local Rule = require 'nvim-autopairs.rule'
-    local conds = require 'nvim-autopairs.conds'
-
-    npairs.setup()
-
-    -- Autoclosing angle-brackets.
-    npairs.add_rule(Rule('<', '>', {
-      -- Avoid conflicts with nvim-ts-autotag.
-      '-html',
-      '-javascriptreact',
-      '-typescriptreact',
-    }):with_pair(conds.before_regex('%a+:?:?$', 3)):with_move(function(opts)
-      return opts.char == '>'
-    end))
-
-    -- remove add ` char on filetype markdown
-    local getRule = require('nvim-autopairs').get_rules
-    getRule('`')[1].not_filetypes = { 'markdown' }
-  end,
+  -- 'windwp/nvim-autopairs',
+  -- event = { 'InsertEnter' },
+  -- config = function()
+  --   local npairs = require 'nvim-autopairs'
+  --   local Rule = require 'nvim-autopairs.rule'
+  --   local conds = require 'nvim-autopairs.conds'
+  --
+  --   npairs.setup()
+  --
+  --   -- Autoclosing angle-brackets.
+  --   npairs.add_rule(Rule('<', '>', {
+  --     -- Avoid conflicts with nvim-ts-autotag.
+  --     '-html',
+  --     '-javascriptreact',
+  --     '-typescriptreact',
+  --   }):with_pair(conds.before_regex('%a+:?:?$', 3)):with_move(function(opts)
+  --     return opts.char == '>'
+  --   end))
+  --
+  --   -- remove add ` char on filetype markdown
+  --   local getRule = require('nvim-autopairs').get_rules
+  --   getRule('`')[1].not_filetypes = { 'markdown' }
+  -- end,
 }
