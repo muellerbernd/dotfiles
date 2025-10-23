3a615465243039d167eb0c3bba5844c957debb5c
Bernd Müller <github@muellerbernd.de>
2025-10-23 08:39:36 +0200

    [refactor] nvim: presenterm settings

--- nvim/.config/nvim/lua/bemu/plugins/present.lua
+++ nvim/.config/nvim/lua/bemu/plugins/present.lua
@@ -1,19 +1,19 @@
 return {
   -- 'tjdevries/present.nvim'
-  -- {
-  --   'Piotr1215/presenterm.nvim',
-  --   build = false,
-  --   config = function()
-  --     require('presenterm').setup {
-  --       default_keybindings = true,
-  --       picker = {
-  --         provider = 'telescope', -- Options: "telescope", "fzf", "snacks", "builtin"
-  --       },
-  --       preview = {
-  --         command = 'presenterm -xX',
-  --         presentation_preview_sync = true,
-  --       },
-  --     }
-  --   end,
-  -- },
+  {
+    'Piotr1215/presenterm.nvim',
+    build = false,
+    config = function()
+      require('presenterm').setup {
+        default_keybindings = true,
+        picker = {
+          provider = 'telescope', -- Options: "telescope", "fzf", "snacks", "builtin"
+        },
+        preview = {
+          command = 'presenterm -xX',
+          presentation_preview_sync = true,
+        },
+      }
+    end,
+  },
 }
