a160093128a1f2bf7633f078f0bcf5ad52d7e217
Bernd Müller <github@muellerbernd.de>
2025-12-11 14:15:21 +0100

    [change](nvim): treesitter context

--- nvim/.config/nvim/lua/bemu/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/bemu/plugins/nvim-treesitter.lua
@@ -112,23 +112,23 @@ return {
       -- Disable it when the window is too small.
       min_window_height = 20,
     },
-    keys = {
-      {
-        '[c',
-        function()
-          -- Jump to previous change when in diffview.
-          if vim.wo.diff then
-            return '[c'
-          else
-            vim.schedule(function()
-              require('treesitter-context').go_to_context()
-            end)
-            return '<Ignore>'
-          end
-        end,
-        desc = 'Jump to upper context',
-        expr = true,
-      },
-    },
+    -- keys = {
+    --   {
+    --     '[c',
+    --     function()
+    --       -- Jump to previous change when in diffview.
+    --       if vim.wo.diff then
+    --         return '[c'
+    --       else
+    --         vim.schedule(function()
+    --           require('treesitter-context').go_to_context()
+    --         end)
+    --         return '<Ignore>'
+    --       end
+    --     end,
+    --     desc = 'Jump to upper context',
+    --     expr = true,
+    --   },
+    -- },
   },
 }
